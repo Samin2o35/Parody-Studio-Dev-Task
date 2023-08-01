a579ae5e8855671c732f7016d0e071e6da6faed1
Samin2o35 <60335026+Samin2o35@users.noreply.github.com>
2023-08-01 19:41:22 +0600

    Added Time limit
    -Added countdown timer that counts down from 2 minutes
    
    Bug Fix
    - Character no longer falls awkwardly when jumping
    
    Bug
    -Nothing happens at timer end

---
+++ UnityDeveloper_Test/Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeLeft;
+    public bool timerOn = false;
+    public TextMeshProUGUI timerText; 
+
+    void Start()
+    {
+        timerOn = true;
+    }
+
+    void Update()
+    {
+        if (timerOn)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                UpdateTimer(timeLeft);
+            }
+            else
+            {
+                timeLeft = 0;
+                timerOn = false;
+            }
+        }
+    }
+
+    void UpdateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+    }
+}
